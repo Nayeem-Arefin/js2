a2f4c94c6b101b733e9f3c4c36bb38e97e45834d
Nayeem Arefin <137292799+Nayeem-Arefin@users.noreply.github.com>
2023-09-12 22:44:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function firstText(){ 
+document.getElementById('h1').innerHTML="Hello World";
+}
+function secondText(){
+document.getElementById('h2').innerHTML="This is the successful second test.";
+}
+function thirdText(){
+document.getElementById('h3').innerHTML="This text has been generated after double-clicking.";
+}
+function fourthText (){
+document.getElementById('h4').innerHTML="This text is the 4th successful test.";
+}
+function fifthText (){
+document.getElementById('h5').innerHTML="fifth successful test.";
+}
+function sixthTest(){
+	document.getElementById('h6').innerHTML="sixth successful test.";
+}
+function seventhTest(){
+document.getElementById('heading').innerHTML="seventh successful test";
+}
+function eighthTest(){
+	document.getElementById('headingstart').innerHTML="The 8th test has been successful.;"
+}
+
+function ninethConsole (){
+console.log("The 9th test, if successful, should be displayed in the source section.");
+}
+
+function tenAlert (){
+	alert("The tenth test using the alert keyword has been successful."); 
+}
